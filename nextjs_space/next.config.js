3a850647a25e13605adf0fbd9edae04246d14f80
DeepAgent <deepagent@abacus.ai>
2025-12-19 01:44:32 +0000

    fix: attempt to resolve database idle timeout

--- nextjs_space/next.config.js
+++ nextjs_space/next.config.js
@@ -1,12 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../'),
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   images: { unoptimized: true },
 };
